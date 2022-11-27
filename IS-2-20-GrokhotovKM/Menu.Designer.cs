a31b2adf9858287bb302273b9853b18dea93f356
Grumm <kirill7777xp@mail.ru>
2022-11-27 08:24:57 +0500

    Добавьте файлы проекта.

---
+++ IS-2-20-GrokhotovKM/Menu.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace IS_2_20_GrokhotovKM
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(258, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(654, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Инициализировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(654, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Вывести";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 82);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(159, 29);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 117);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(159, 29);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 152);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(159, 29);
+            this.textBox5.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Жёсткий диск",
+            "Видеокарта"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 395);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 187);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(159, 29);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 222);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(159, 29);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 257);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(159, 29);
+            this.textBox8.TabIndex = 11;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 450);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ IS-2-20-GrokhotovKM/Menu.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace IS_2_20_GrokhotovKM
+{
+    public partial class Menu : Form
+    {
+        //static string connect = "server=chuc.caseum.ru; port=33333; database=is_2_20_st8_KURS; user=st_2_20_8; password=82411770";
+        internal HardDisk hd;
+        internal Graphic gp;
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        
+        public abstract class Access<T>
+        {
+            T Articul;
+            internal decimal Price;
+            internal short Year;
+            public Access(decimal price, short year)
+            {
+                Price = price;
+                Year = year;
+            }
+            //public Access()
+            //{
+
+            //}
+            public abstract void Display(ListBox listbox);
+        }
+
+
+        internal class HardDisk : Access<string>
+        {
+            //Вращения
+            private int turns;
+            public int Turns_Sv
+            {
+                get { return turns; }
+                set { turns = value; }
+            }
+            //
+            //Интерфейс(поле)
+            private string @interface;
+            public string Interface_Sv
+            {
+                get { return @interface; }
+                set { @interface = value; } 
+            }
+            //
+            //Объём
+            private int size;
+            public int Size_Sv
+            {
+                get { return size; }
+                set { size = value; }
+            }
+            //
+
+            public HardDisk(decimal price, short year, int turns, string @interface, int size) :base(price, year)
+            {
+                Turns_Sv = turns;
+                Interface_Sv = @interface;
+                Size_Sv = size;
+            }
+
+            public override void Display(ListBox listbox)
+            {
+                listbox.Items.Add(String.Concat(Enumerable.Repeat("=", 20)));
+                listbox.Items.Add($"\nЦена: {this.Price} \tГод: {this.Year}");
+                listbox.Items.Add($"Вращения: {Turns_Sv}\tИнтерфейс: {Interface_Sv}\tОбъём: {Size_Sv}");
+                listbox.Items.Add(String.Concat(Enumerable.Repeat("=", 20)));
+            }
+        }
+        internal class Graphic : Access <string>
+        {
+            //Частота
+            private double gpu_gh;
+            public double GPU_Gh
+            {
+                get { return gpu_gh; }
+                set { gpu_gh = value; }
+            }
+            //
+
+            //Производитель
+            private string maker;
+            public string Maker
+            {
+                get { return maker; }
+                set { maker = value; }
+            }
+            //
+
+            //Объём памяти
+            private short gpu_memory;
+            public short GPU_Memory
+            {
+                get { return gpu_memory; }
+                set { gpu_memory = value; }
+            }
+            //
+
+            public Graphic(decimal price, short year, double gPU_Gh, string maker, short gPU_Memory):base(price, year)
+            {
+                GPU_Gh = gPU_Gh;
+                Maker = maker;
+                GPU_Memory = gPU_Memory;
+            }
+
+            public override void Display(ListBox listbox)
+            {
+                listbox.Items.Add(String.Concat(Enumerable.Repeat("=", 20)));
+                listbox.Items.Add($"\nЦена: {this.Price} \tГод: {this.Year}");
+                listbox.Items.Add($"Частота: {GPU_Gh}\tПроизводитель: {Maker}\tОбъём: {GPU_Memory}");
+                listbox.Items.Add(String.Concat(Enumerable.Repeat("=", 20)));
+
+            }
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            Block();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Жёсткий диск")
+            {
+                hd = new HardDisk(Convert.ToDecimal(textBox1.Text), Convert.ToInt16(textBox2.Text), Convert.ToInt32(textBox3.Text), textBox4.Text, Convert.ToInt32(textBox5.Text));
+            }
+            else if (comboBox1.Text == "Видеокарта")
+            {
+                gp = new Graphic(Convert.ToDecimal(textBox1.Text), Convert.ToInt16(textBox2.Text), Convert.ToDouble(textBox6.Text), textBox7.Text, Convert.ToInt16(textBox8.Text));
+            }
+            MessageBox.Show("Инициализировано");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Жёсткий диск")
+            {
+                hd.Display(listBox1);
+            }
+            else if (comboBox1.Text == "Видеокарта")
+            {
+                gp.Display(listBox1);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Жёсткий диск")
+            {
+                Block();
+                textBox1.Enabled = true; textBox2.Enabled = true; textBox3.Enabled = true; textBox4.Enabled = true; textBox5.Enabled = true; button1.Enabled = true; button2.Enabled = true;
+            }
+            else if(comboBox1.Text == "Видеокарта")
+            {
+                Block();
+                textBox1.Enabled = true; textBox2.Enabled = true; textBox6.Enabled = true; textBox7.Enabled = true; textBox8.Enabled = true; button1.Enabled = true; button2.Enabled = true;
+            }
+            else if (comboBox1.Text == "")
+            {
+                Block();
+            }
+        }
+        internal void Block()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c.Name != "comboBox1")
+                {
+                    c.Enabled = false;
+                    if (!c.Name.Contains("button"))
+                    {
+                        c.Text = "";
+                    }
+                    
+                }
+            }
+            listBox1.Items.Clear();
+        }
+    }
+}
