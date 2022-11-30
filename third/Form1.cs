6fc70c2873fa57c95eaa2985424779e4d790c59e
Grumm <kirill7777xp@mail.ru>
2022-11-30 08:29:25 +0500

    third

---
+++ third/Form1.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace third
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string connStr = "server=chuc.caseum.ru;port=33333;user=st_2_20_8;database=is_2_20_st8_KURS;password=82411770;";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+            ConnectToDb.Page(dataGridView1, conn);
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ConnectToDb.InsertData(connStr));
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show($"{dataGridView1[e.ColumnIndex, e.RowIndex].Value}");
+        }
+    }
+}
---
+++ third/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace third
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        
+    }
+    public class ConnectToDb
+    {
+        public static string InsertData(string connect)
+        {
+            return connect;
+        }
+        public static void Page(DataGridView datagrid, MySqlConnection conn)
+        {
+            string load = $"select * from Main left join t_provider on Main.provider_id=t_provider.id";   
+            MySqlCommand cmd = new MySqlCommand(load, conn);
+            MySqlDataReader read = cmd.ExecuteReader();
+            List<string[]> data = new List<string[]>();
+            while (read.Read())
+            {
+                data.Add(new string[7]);
+                data[data.Count - 1][0] = read[0].ToString();
+                data[data.Count - 1][1] = read[1].ToString();
+                data[data.Count - 1][2] = read[2].ToString();
+                data[data.Count - 1][3] = read[3].ToString();
+                data[data.Count - 1][4] = read[4].ToString();
+                data[data.Count - 1][5] = read[5].ToString();
+                data[data.Count - 1][6] = read[6].ToString();
+            }
+            foreach (string[] s in data)
+                datagrid.Rows.Add(s);
+
+            read.Close();
+        }
+    }
+}
